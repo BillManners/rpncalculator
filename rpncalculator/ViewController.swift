bc9e0a18fc7606524dfd69993d64b29371c5130b
BillManners <manners.w@etoncollege.org.uk>
2020-06-10 22:44:17 +0100

    Display and input fully functional

--- rpncalculator/ViewController.swift
+++ rpncalculator/ViewController.swift
@@ -9,12 +9,109 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var zero: UIButton!
+    @IBOutlet var one: UIButton!
+    @IBOutlet var two: UIButton!
+    @IBOutlet var three: UIButton!
+    @IBOutlet var four: UIButton!
+    @IBOutlet var five: UIButton!
+    @IBOutlet var six: UIButton!
+    @IBOutlet var seven: UIButton!
+    @IBOutlet var eight: UIButton!
+    @IBOutlet var nine: UIButton!
+    @IBOutlet var evaluate: UIButton!
+    @IBOutlet var clear: UIButton!
+    @IBOutlet var enter: UIButton!
+    @IBOutlet var chs: UIButton!
+    @IBOutlet var add: UIButton!
+    @IBOutlet var subtract: UIButton!
+    @IBOutlet var multiply: UIButton!
+    @IBOutlet var divide: UIButton!
+    
+    @IBOutlet var calcDisplay: UILabel!
+    var primaryStack = Stack()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
     }
-
-
+    
+    @IBAction func zeroPressed(_ sender: Any) {
+        primaryStack.appendThis(toAppend: "0")
+        calcDisplay.text = primaryStack.display()
+    }
+    @IBAction func onePressed(_ sender: Any) {
+        
+        primaryStack.appendThis(toAppend: "1")
+        calcDisplay.text = primaryStack.display()
+    }
+    @IBAction func twoPressed(_ sender: Any) {
+        primaryStack.appendThis(toAppend: "2")
+        calcDisplay.text = primaryStack.display()
+    }
+    @IBAction func threePressed(_ sender: Any) {
+        primaryStack.appendThis(toAppend: "3")
+        calcDisplay.text = primaryStack.display()
+    }
+    @IBAction func fourPressed(_ sender: Any) {
+        primaryStack.appendThis(toAppend: "4")
+        calcDisplay.text = primaryStack.display()
+    }
+    @IBAction func fivePressed(_ sender: Any) {
+        primaryStack.appendThis(toAppend: "5")
+        calcDisplay.text = primaryStack.display()
+    }
+    @IBAction func sixPressed(_ sender: Any) {
+        primaryStack.appendThis(toAppend: "6")
+        calcDisplay.text = primaryStack.display()
+    }
+    @IBAction func sevenPressed(_ sender: Any) {
+        primaryStack.appendThis(toAppend: "7")
+        calcDisplay.text = primaryStack.display()
+    }
+    @IBAction func eightPressed(_ sender: Any) {
+        primaryStack.appendThis(toAppend: "8")
+        calcDisplay.text = primaryStack.display()
+    }
+    @IBAction func ninePressed(_ sender: Any) {
+        primaryStack.appendThis(toAppend: "9")
+        calcDisplay.text = primaryStack.display()
+    }
+    @IBAction func evaluatePressed(_ sender: Any) {
+        primaryStack.endOfOperand()
+        //Actual coding
+    }
+    @IBAction func clearPressed(_ sender: Any) {
+        primaryStack.deleteAll()
+        calcDisplay.text = primaryStack.display()
+    }
+    @IBAction func enterPressed(_ sender: Any) {
+        primaryStack.endOfOperand()
+        calcDisplay.text = primaryStack.display()
+    }
+    @IBAction func chsPressed(_ sender: Any) {
+        primaryStack.appendThis(toAppend: "~")
+        calcDisplay.text = primaryStack.display()
+    }
+    @IBAction func addPressed(_ sender: Any) {
+        primaryStack.appendThis(toAppend: "+")
+        calcDisplay.text = primaryStack.display()
+    }
+    @IBAction func subtractPressed(_ sender: Any) {
+        primaryStack.appendThis(toAppend: "-")
+        calcDisplay.text = primaryStack.display()
+    }
+    
+    @IBAction func multiplyPressed(_ sender: Any) {
+        primaryStack.appendThis(toAppend: "*")
+        calcDisplay.text = primaryStack.display()
+    }
+    @IBAction func dividePressed(_ sender: Any) {
+        primaryStack.appendThis(toAppend: "/")
+        calcDisplay.text = primaryStack.display()
+    }
+    
 }
 
---
+++ rpncalculator/stackCreation.swift
@@ -0,0 +1,55 @@
+//
+//  stackCreation.swift
+//  rpncalculator
+//
+//  Created by Manners, William (Coll) on 09/06/2020.
+//  Copyright © 2020 Manners, William (Coll). All rights reserved.
+//
+
+import Foundation
+
+class Stack{
+    var stackContents: [String]
+    var currentOperand: String
+    init(){
+        stackContents = []
+        currentOperand = ""
+    }
+    func appendThis(toAppend: String){
+        if toAppend == "+" || toAppend == "-" || toAppend == "*" || toAppend == "/"{
+            self.endOfOperand()
+            currentOperand = toAppend
+            self.endOfOperand()
+        } else if toAppend == "~"{
+            currentOperand = toAppend + currentOperand
+        }else if currentOperand.count < 3 || (currentOperand.count < 4 && currentOperand.contains("~")){
+            currentOperand = currentOperand + toAppend
+        } else {
+            self.endOfOperand()
+        }
+        
+    }
+    
+    func endOfOperand(){
+        if currentOperand.count != 0{
+            self.stackContents.insert(self.currentOperand, at: 0)
+            self.currentOperand = ""
+        }
+    }
+    
+    func display()->String{
+        var displayMessage = ""
+        let lengthOfStackContents = stackContents.count
+        for i in (0..<(stackContents.count)){
+            displayMessage = displayMessage + stackContents[lengthOfStackContents-i-1]
+        }
+        displayMessage = displayMessage + currentOperand
+        return displayMessage
+    }
+    
+    func deleteAll(){
+        self.currentOperand = ""
+        self.stackContents = []
+    }
+    
+}
